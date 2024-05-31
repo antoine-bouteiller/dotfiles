5e16fda8581861df6b2bb2af8b0df177ec4beeba
Antoine <anto.bouteiller@gmail.com>
2024-05-31 19:13:07 +0200

    feat(zsh): update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,12 +1,9 @@
 alias ll="ls -all"
 alias zshconfig="vim ~/.zshrc"
-alias ga="git add ./"
-alias gc='git commit'
-alias gp='git push --force-with-lease'
-alias git-clean='git fetch -p && for branch in $(git for-each-ref --format '\''%(refname) %(upstream:track)'\'' refs/heads | awk '\''$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'\''); do git branch -D $branch; done'
+alias gclean='git fetch -p && for branch in $(git for-each-ref --format '\''%(refname) %(upstream:track)'\'' refs/heads | awk '\''$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'\''); do git branch -D $branch; done'
 
 # Update node versions
-function nvm_update() {
+function nvmu() {
     local prev_ver=$(nvm version ${1:-"lts/*"})
     nvm install ${1:-"lts/*"} --latest-npm
     if [ "$prev_ver" != "$(nvm current)" ]; then

5aa74e17482bdfdb0e5cb22650f7ddfc87e81c78
Antoine Bouteiller <antoine.b@galadrim.fr>
2024-10-13 10:59:55 +0200

    feat(brew): fix update all alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,4 +3,4 @@ alias zshconfig="vim ~/.zshrc"
 alias gclean='git fetch -p && for branch in $(git for-each-ref --format '\''%(refname) %(upstream:track)'\'' refs/heads | awk '\''$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'\''); do git branch -D $branch; done'
 alias gps='git checkout staging && git rebase develop && gpf!'
 alias gpp='git checkout production && git rebase staging && gpf!'
-alias bua='bup && bcup && bcn'
+alias bua='bup && bcup --greedy && bcn'

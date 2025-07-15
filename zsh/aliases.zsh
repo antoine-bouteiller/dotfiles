17dfddc3c4ff6c079820d762ffd6664c0f3f148d
Antoine Bouteiller <anto.bouteiller@gmail.com>
2025-07-15 14:50:18 +0200

    feat: use force with lease

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,6 @@
 alias ll="ls -all"
 alias zshconfig="vim ~/.zshrc"
 alias gclean='git fetch -p && for branch in $(git for-each-ref --format '\''%(refname) %(upstream:track)'\'' refs/heads | awk '\''$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'\''); do git branch -D $branch; done'
-alias gps='git checkout staging && git rebase develop && gpf!'
-alias gpp='git checkout production && git rebase staging && gpf!'
+alias gps='git checkout staging && git rebase develop && gpf'
+alias gpp='git checkout production && git rebase staging && gpf'
 alias bua='bup && bcup --greedy && bcn'

662721864895074218e1fd211dc64375a297d2b4
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2025-12-05 19:39:29 +0100

    feat: install zed on setup

--- install-windows.ps1
+++ install-windows.ps1
@@ -4,13 +4,13 @@ $ErrorActionPreference = "Stop"
 
 $dotfilesDir = "$env:USERPROFILE\.dotfiles"
 
-$packages = @("Nushell.Nushell", "Starship.Starship", "sharkdp.bat", "ajeetdsouza.zoxide", "rsteube.Carapace", "jdx.mise)
+$packages = @("Nushell.Nushell", "Starship.Starship", "sharkdp.bat", "ajeetdsouza.zoxide", "rsteube.Carapace", "jdx.mise", "ZedIndustries.Zed")
 foreach ($package in $packages) {
     winget install --id $package --silent --accept-source-agreements --accept-package-agreements
 }
 
 $nuConfigDir = & nu -c '$nu.default-config-dir'
-$zedDir = "$env:APPDATA\zed"
+$zedDir = "$env:APPDATA\Zed"
 
 if ($nuConfigDir -ne "$dotfilesDir\nushell") {
     if (Test-Path $nuConfigDir) {

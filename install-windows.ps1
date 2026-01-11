70d00bbfef95f7b91cedd54336c94e456454ab2e
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2026-01-11 16:10:25 +0100

    feat: update windows install script

--- install-windows.ps1
+++ install-windows.ps1
@@ -4,21 +4,23 @@ $ErrorActionPreference = "Stop"
 
 $dotfilesDir = "$env:USERPROFILE\.dotfiles"
 
-$packages = @("Nushell.Nushell", "Starship.Starship", "sharkdp.bat", "ajeetdsouza.zoxide", "jdx.mise", "ZedIndustries.Zed")
+$packages = @("Starship.Starship", "sharkdp.bat", "ajeetdsouza.zoxide", "jdx.mise", "ZedIndustries.Zed")
 foreach ($package in $packages) {
     winget install --id $package --silent --accept-source-agreements --accept-package-agreements
 }
 
-$nuConfigDir = & nu -c '$nu.default-config-dir'
+$powershellProfileDir = Split-Path -Parent $PROFILE
+$powershellProfile = $PROFILE
 $zedDir = "$env:APPDATA\Zed"
 
-if ($nuConfigDir -ne "$dotfilesDir\nushell") {
-    if (Test-Path $nuConfigDir) {
-        Remove-Item -Recurse -Force $nuConfigDir
-    }
-    New-Item -ItemType SymbolicLink -Path $nuConfigDir -Target "$dotfilesDir\nushell" -Force
+if (-not (Test-Path $powershellProfileDir)) {
+    New-Item -ItemType Directory -Path $powershellProfileDir -Force
 }
 
+if (Test-Path $powershellProfile) {
+    Remove-Item -Force $powershellProfile
+}
+New-Item -ItemType SymbolicLink -Path $powershellProfile -Target "$dotfilesDir\powershell\profile.ps1" -Force
 
 if (Test-Path $zedDir) {
     Remove-Item -Recurse -Force $zedDir

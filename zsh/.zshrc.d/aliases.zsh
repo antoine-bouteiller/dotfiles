32ee3e47e6813a50e3085c81eed5f0b537ff594a
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2026-02-08 15:04:47 +0100

    chore: remove .DS_Store and remove unused alias

--- zsh/.zshrc.d/aliases.zsh
+++ zsh/.zshrc.d/aliases.zsh
@@ -14,8 +14,6 @@ alias ldot='ls -ld .*'
 
 # more git alias
 alias gclean='git fetch -p && for branch in $(git for-each-ref --format '\''%(refname) %(upstream:track)'\'' refs/heads | awk '\''$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'\''); do git branch -D $branch; done'
-alias gps='git checkout staging && git rebase develop && gpf'
-alias gpp='git checkout production && git rebase staging && gpf'
 
 # fix common typos
 alias quit='exit'

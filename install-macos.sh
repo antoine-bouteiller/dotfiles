85c088264afdcd93cf6ce6a213d87b9539e94b1a
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2026-02-16 16:57:01 +0100

    feat: update install script and add git config

--- install-macos.sh
+++ install-macos.sh
@@ -9,22 +9,20 @@ if ! command -v brew &> /dev/null; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-packages=("nushell" "starship" "bat" "zoxide" "carapace" "fish" "mise" "ghostty" "zed")
+packages=("starship" "bat" "zoxide" "carapace" "fish" "mise" "ghostty" "zed")
 
 for package in "${packages[@]}"; do
     brew install "$package"
 done
 
-if [ "$server_mode" = false ]; then
-    ghostty_dir="${HOME}/.config/ghostty"
-    zed_dir="${HOME}/.config/zed"
+ghostty_dir="${HOME}/.config/ghostty"
+zed_dir="${HOME}/.config/zed"
 
-    mkdir -p "$ghostty_dir"
-    rm -f "$ghostty_dir/config"
-    ln -s "${DOTDIR}/ghostty" "$ghostty_dir/config"
+mkdir -p "$ghostty_dir"
+rm -f "$ghostty_dir/config"
+ln -s "${DOTDIR}/ghostty" "$ghostty_dir/config"
 
-    rm -rf "$zed_dir"
-    ln -s "${DOTDIR}/zed" "$zed_dir"
-fi
+rm -rf "$zed_dir"
+ln -s "${DOTDIR}/zed" "$zed_dir"
 
 echo "source $DOTDIR/zsh/.zshenv" >> "$HOME/.zshenv"

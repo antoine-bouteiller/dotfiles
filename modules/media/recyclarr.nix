1fc0f8750501612cb9269fe0da2c57d0f87496ae
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2026-04-12 16:31:19 +0200

    feat: udpate recyclarr config to v8

--- modules/media/recyclarr.nix
+++ modules/media/recyclarr.nix
@@ -34,27 +34,43 @@ in {
             };
 
             delete_old_custom_formats = true;
-            replace_existing_custom_formats = true;
 
-            include = [
-              {template = "sonarr-v4-quality-profile-web-1080p";}
-              {template = "sonarr-v4-custom-formats-web-1080p";}
+            quality_definition = {
+              type = "series";
+              preferred_ratio = "0.5";
+            };
 
-              {template = "sonarr-v4-quality-profile-anime";}
-              {template = "sonarr-v4-custom-formats-anime";}
-            ];
-
-            custom_formats = [
+            quality_profiles = [
               {
-                trash_ids = ["47435ece6b99a0b477caf360e79ba0bb"]; # x265 (HD)
-                assign_scores_to = [
-                  {
-                    name = "WEB-1080p";
-                    score = 0;
-                  }
-                ];
+                trash_id = "72dae194fc92bf828f32cde7744e51a1"; # WEB-1080p
+              }
+              {
+                trash_id = "72dae194fc92bf828f32cde7744e51a1"; # [Anime] Remux-1080p
               }
             ];
+
+            custom_format_groups = {
+              add = [
+                {
+                  trash_id = "f4a0410a1df109a66d6e47dcadcce014"; # [Optional] Miscellaneous
+                  select = [
+                    "32b367365729d530ca1c124a0b180c64" # Bad Dual Groups
+                    "ef4963043b0987f8485bc9106f16db38" # DV (Disk)
+                    "1bd69272e23c5e6c5b1d6c8a36fce95e" # HFR
+                    "7ba05c6e0e14e793538174c679126996" # MULTi
+                    "82d40da2bc6923f41e14394075dd4b03" # No-RlsGroup
+                    "e1a997ddb54e3ecbfe06341ad323c458" # Obfuscated
+                    "06d66ab109d4d2eddb2794d21526d140" # Retags
+                    "1b3994c551cbb92a2c781af061f4ab44" # Scene
+                    "7470a681e6205243983c4410ee4c920f" # VC-1
+                    "90501962793d580d011511155c97e4e5" # VP9
+                    "cddfb4e32db826151d97352b8e37c648" # x264
+                    "c9eafd50846d299b862ca9bb6ea91950" # x265
+                    "041d90b435ebd773271cea047a457a6a" # x266
+                  ];
+                }
+              ];
+            };
           };
         };
 
@@ -66,31 +82,61 @@ in {
             };
 
             delete_old_custom_formats = true;
-            replace_existing_custom_formats = true;
+
+            quality_definition = {
+              type = "sqp-streaming";
+              qualities = [
+                {
+                  name = "WEBDL-1080p";
+                  min = 15;
+                  preferred = 30;
+                  max = 100;
+                }
+                {
+                  name = "WEBRip-1080p";
+                  min = 15;
+                  preferred = 30;
+                  max = 100;
+                }
+                {
+                  name = "Bluray-1080p";
+                  min = 50;
+                  preferred = 75;
+                  max = 150;
+                }
+              ];
+            };
 
             quality_profiles = [
               {
-                name = "SQP-1 WEB (1080p)";
+                trash_id = "90a3370d2d30cbaf08d9c23b856a12c8"; # [SQP] SQP-1 WEB (1080p)
                 min_format_score = 10;
               }
             ];
 
-            include = [
-              {template = "radarr-quality-profile-sqp-1-web-1080p";}
-              {template = "radarr-custom-formats-sqp-1-web-1080p";}
-            ];
-
-            custom_formats = [
-              {
-                trash_ids = ["dc98083864ea246d05a42df0d05f81cc"]; # x265 (HD)
-                assign_scores_to = [
-                  {
-                    name = "SQP-1 WEB (1080p)";
-                    score = 0;
-                  }
-                ];
-              }
-            ];
+            custom_format_groups = {
+              add = [
+                {
+                  trash_id = "9337080378236ce4c0b183e35790d2a7"; # [Optional] Miscellaneous
+                  select = [
+                    "b6832f586342ef70d9c128d40c07b872" # Bad Dual Groups
+                    "cc444569854e9de0b084ab2b8b1532b2" # Black and White Editions
+                    "f700d29429c023a5734505e77daeaea7" # DV (Disk)
+                    "73613461ac2cea99d52c4cd6e177ab82" # HFR
+                    "4b900e171accbfb172729b63323ea8ca" # Multi
+                    "ae9b7c9ebde1f3bd336a8cbd1ec4c5e5" # No-RlsGroup
+                    "7357cf5161efbf8c4d5d0c30b4815ee2" # Obfuscated
+                    "5c44f52a8714fdd79bb4d98e2673be1f" # Retags
+                    "f537cf427b64c38c8e36298f657e4828" # Scene
+                    "11cd1db7165d6a7ad9a83bc97b8b1060" # VC-1
+                    "ae4cfaa9283a4f2150ac3da08e388723" # VP9
+                    "2899d84dc9372de3408e6d8cc18e9666" # x264
+                    "9170d55c319f4fe40da8711ba9d8050d" # x265
+                    "390455c22a9cac81a738f6cbad705c3c" # x266
+                  ];
+                }
+              ];
+            };
           };
         };
       };

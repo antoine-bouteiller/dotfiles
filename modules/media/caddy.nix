aa587384c0b25ac4daf08e3e12f3b862a648b03e
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2026-04-13 16:44:27 +0200

    feat: add cloudflare cloudflare ips to trusted proxies in caddy

--- modules/media/caddy.nix
+++ modules/media/caddy.nix
@@ -4,20 +4,34 @@
   ...
 }: let
   cfg = config.mediaServer;
+  cloudflareIpsV4File = builtins.fetchurl {
+    url = "https://www.cloudflare.com/ips-v4";
+    sha256 = "sha256-8Cxtg7wBqwroV3Fg4DbXAMdFU1m84FTfiE5dfZ5Onns="; # Note: builtins.fetchurl uses 'sha256' instead of 'hash'
+  };
+  cloudflareIps =
+    builtins.filter (s: s != "")
+    (lib.splitString "\n" (builtins.readFile cloudflareIpsV4File));
+  trustedProxies = builtins.concatStringsSep " " cloudflareIps;
 in {
   config = lib.mkIf cfg.enable {
     services.caddy = {
-    enable = true;
+      enable = true;
 
-    extraConfig = ''
-      (auth_proxy) {
-        forward_auth localhost:${toString cfg.authelia.port} {
-          uri /api/authz/forward-auth
-          copy_headers Remote-User Remote-Groups Remote-Email Remote-Name
-          header_down -Authorization
+      globalConfig = ''
+        servers {
+          trusted_proxies static ${trustedProxies}
         }
-      }
-    '';
-  };
+      '';
+
+      extraConfig = ''
+        (auth_proxy) {
+          forward_auth localhost:${toString cfg.authelia.port} {
+            uri /api/authz/forward-auth
+            copy_headers Remote-User Remote-Groups Remote-Email Remote-Name
+            header_down -Authorization
+          }
+        }
+      '';
+    };
   };
 }

87c52fd21c12c8bac36389534aefc771356c7940
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2026-04-22 11:33:25 +0200

    feat: cleanup desktop

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -11,12 +11,15 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    services.displayManager.sddm.enable = true;
-    services.displayManager.sddm.wayland.enable = true;
+    services.displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+      theme = "WhiteSur-dark";
+    };
     services.desktopManager.plasma6.enable = true;
 
     environment.systemPackages = with pkgs; [
-      inter
+      whitesur-kde
     ];
   };
 }

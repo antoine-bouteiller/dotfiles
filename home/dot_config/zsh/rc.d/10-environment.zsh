1d2f80dba01653f5b5f949987e864957807ab36e
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2026-08-23 17:17:32 +0200

    feat(bitwarden): configure SSH agent socket

--- home/dot_config/zsh/rc.d/10-environment.zsh
+++ home/dot_config/zsh/rc.d/10-environment.zsh
@@ -6,3 +6,8 @@ export path=(
   /usr/local/{,s}bin(N)
   $path
 )
+
+case $OSTYPE in
+  darwin*) export SSH_AUTH_SOCK="$HOME/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock" ;;
+  linux*) export SSH_AUTH_SOCK="$HOME/.bitwarden-ssh-agent.sock" ;;
+esac

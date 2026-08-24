233a0aafe184bc4f462ae250944aefed52054633
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2026-08-24 09:49:14 +0200

    refactor(zsh): drop catppuccin p10k theme in favor of ~/.p10k.zsh
    Remove the catppuccin-powerlevel10k-themes external and its dark-mode autoswitch hook, sourcing the plain p10k config instead.

---
+++ home/dot_config/zsh/rc.d/50-prompt.zsh
@@ -0,0 +1,4 @@
+if [[ -f "$HOME/.zsh/plugins/zsh-powerlevel10k/powerlevel10k.zsh-theme" ]]; then
+  source "$HOME/.zsh/plugins/zsh-powerlevel10k/powerlevel10k.zsh-theme"
+  [[ -f "$HOME/.p10k.zsh" ]] && source "$HOME/.p10k.zsh"
+fi

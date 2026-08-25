10f3b49530bfe33e0f2dd87c5eef030c35262a37
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2026-08-25 14:18:16 +0200

    ci: push and pull builds through the antoine-bouteiller cachix cache

--- hosts/base.nix
+++ hosts/base.nix
@@ -18,11 +18,13 @@
       keep-derivations = true;
       substituters = [
         "https://nix-community.cachix.org"
+        "https://antoine-bouteiller.cachix.org"
         "https://cache.nixos.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "antoine-bouteiller.cachix.org-1:vtMtdYM8LJ3CRDrqiTllxXRnu4x3xwYOb6317TUJUTc="
       ];
     };
 

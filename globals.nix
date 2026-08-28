ad928c8a7b58636c286a8ae3605f021fb5fb12f5
Antoine Bouteiller <115460763+antoine-bouteiller@users.noreply.github.com>
2026-08-28 19:47:01 +0200

    chore(keys): replace the dell host key
    Swap the old DESKTOP-3R8RBJU key for the new antoine-dell key in both the sops recipients and the authorized ssh keys.

--- globals.nix
+++ globals.nix
@@ -4,7 +4,7 @@
   email = "115460763+antoine-bouteiller@users.noreply.github.com";
   sshKeys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICUZ+rb5WpsUv/4wVWlQ0aCRiNzZCIQngxXiNAJx6hJs antob@DESKTOP-ANTOINE"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHhSmVRhMXbxDkDOaUk0udibjBos2nDg6byvZ//dzMwL antob@DESKTOP-3R8RBJU"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIfK4sKI0QpEbaADjQm/7bK3DlNY/akOh+6yC+q3aG17 antoine.bouteiller@pelico.io"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJf/EE7y7ua7CVtBHXJqSN+jl5xN+c2hKyjI3oVFG9LT antoinebouteiller@antoine-dell"
   ];
 }
